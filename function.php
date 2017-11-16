88e1c7426ac16551336c44e2b6d615cd6463fce8
legendheng <legendheng@users.noreply.github.com>
2017-11-16 19:21:27 +0800

    Add files via upload

---
+++ function.php
@@ -0,0 +1,19 @@
+<?php
+function template_muban($data,$ACCESS_TOKEN){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "http://api.weixin.qq.com/cgi-bin/message/template/send?access_token=".$ACCESS_TOKEN);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (compatible; MSIE 5.01; Windows NT 5.0)');
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_AUTOREFERER, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $tmpInfo = curl_exec($ch);
+    if (curl_errno($ch)) {
+        return curl_error($ch);
+    }
+    curl_close($ch);
+    return $tmpInfo;
+}
